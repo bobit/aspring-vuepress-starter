8a024ce354cfd607d7ef3743d6ae444d7b18a451
bobit <iloveapi@163.com>
2019-09-03 22:10:11 +0800

    update

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="theme-container">
+    <div class="content">
+      <h1>404</h1>
+      <blockquote>{{ getMsg() }}</blockquote>
+      <router-link to="/">Take me home.</router-link>
+    </div>
+  </div>
+</template>
